f11dff1a54fc881a26b85f6e3674a90088b6505e
Rohit Handique <rohit.handique.rh@gmail.com>
2021-05-02 21:10:01 +0530

    updated heroku files

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+[theme]\n\
+base="light"\n\
+primaryColor="#3371f6"\n\
+\n\
+" > ~/.streamlit/config.toml
