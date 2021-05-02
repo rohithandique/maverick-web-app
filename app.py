d3c2532100656892327bc5c1873775bc6c8b619a
Rohit Handique <rohit.handique.rh@gmail.com>
2021-05-03 03:45:40 +0530

    added input features

--- app.py
+++ app.py
@@ -1,28 +1,69 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 st.title('My first app')
 st.write('### Hello World')
-
-st.sidebar.checkbox('hello')
-
-st.sidebar.selectbox('hello', [1,2])
-
-expander = st.beta_expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
-
-left_column, right_column = st.beta_columns(2)
-left_column.button('Press me!')
-with right_column:
-    chosen = st.radio(
-        'Sorting hat',
-        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-    st.write(f"You are in {chosen} house!")
-
+columns = ('POC ID','Total Volume 2018','Total Volume 2019','City Tier','POC Image',
+    'Segment','Sub Segment','Brand','Sub Brand','Returnability',
+    'Pack Type','Gross Turnover 2019','Product Volume 2019',
+    'Tax','Province',)
+df = pd.DataFrame(
+    columns = columns
+)
+df_display = st.table(df)
 with st.form(key='my_form'):
-    text_input = st.text_input(label='Enter your name')
-    x = st.slider('x')
+    poc_id = st.text_input(label='Enter POC ID',key="1")
+    city_tier = st.selectbox('City Tier',[0,1,2],key="2")
+    
+    left_col1, right_col1 = st.beta_columns(2)
+    with left_col1:
+        poc_image = st.radio("POC Image",('Mainstream', 'Premium'),key="5")
+        total_volume_2018 = st.number_input(label='Total Volume 2018',key="3")
+        product_volume_2019 = st.number_input(label='Product Volume 2019',key="4")
+        segment = st.selectbox('Segment',['Entertainment Led', 'Drink Led',
+        'Institutional', 'Food Led', 'Wholesaler', 'Not applicable'],key="8")
+        brand = st.selectbox('Brand',['JUPILER', 'PIEDBOEUF', 'DIEKIRCH', 'BELLE VUE', 'HOEGAARDEN', 'LEFFE', 'TRIPEL KARMELIET', 'GOOSE ISLAND', 'STELLA ARTOIS', 'CORONA', 'VIEUX TEMPS', 'SCOTCH CTS', 'PURE BLONDE', 'CUBANISTO', 'GINETTE', 'KWAK', 'BASS', 'KRUGER', "BECK'S", 'HORSE ALE',
+        'GINDER-ALE', 'DEUS', 'SAFIR', 'BIRRA DEL BORGO'],key="10")
+        pack_type = st.selectbox("Pack Type",['BOTTLE', 'BULK', 'KEG', 'CAN', 'PERFECTDRAFT'],key="15")
+        
+    with right_col1:
+        returnability = st.radio("Returnability",('Returnable', 'Non-Returnable'),key="12")
+        total_volume_2019 = st.number_input(label='Total Volume 2019',key="6")
+        gross_turnover_2019 = st.number_input(label='Gross Turnover 2019',key="7")
+        sub_segment = st.selectbox('Sub Segment',['Events', 'Party Place', 'Institutional', 'Bar', 'Restaurant', 'Sports Venue', 'Recreational', 'Beer bar', 'Hybrid', 'Quick Dining', 'Music Venue', 'Cocktail Bar', 'Wholesaler',
+        'Local Bar', 'Sub Agent', 'Not applicable'],key="9")
+        sub_brand = st.selectbox('Sub Brand',['JUPILER PILS', 'PIEDBOEUF TRIPLE', 'JUPILER 0,0', 'PIEDBOEUF BLONDE', 'PIEDBOEUF FONCEE', 'DIEKIRCH PILS', 'BELLE VUE EXTRA KRIEK', 'HOEGAARDEN ROSEE', 'HOEGAARDEN WHITE', 'FLAVOURED ALCOHOLIC', 'LEFFE BLONDE', 'TRIPEL KARMELIET', 'GOOSE ISLAND IPA', 'HOEGAARDEN ROSE 0,0', 'LEFFE BRUNE', 'LEFFE SANS ALCOOL/ALCOHOLVRIJ', 'STELLA ARTOIS REGULAR', 'BELLE VUE KRIEK CLASSIQUE', 'CORONA EXTRA', 'JUPILER BLUE', 'VIEUX TEMPS REGULAR', 'SCOTCH CTS', 'BELLE VUE GUEUZE', 'HOEG RADLER LEMON 0,0', 'PURE BLONDE REGULAR', 'CUBANISTO RUM', 'GINETTE LAGER', 'GINETTE FRUIT', 'GINETTE TRIPEL', 'KWAK', 'HOEGAARDEN JULIUS', 'LEFFE', 'LEFFE ROYALE', 'LEFFE RUBY', 'LEFFE TRIPLE', 'HOEGAARDEN BEATRIX', 'HOEGAARDEN YELLOW', 'BASS PALE ALE', 'KRUGER EXPORT', 'HOEGAARDEN 0,0', 'LEFFE RITUEL 9', 'HOEGAARDEN GRAND CRU', 'LEFFE ROYALE CASCADE IPA', 'GINETTE BLONDE', 'HOEGAARDEN FORBIDDEN FRUIT', ' HOEGAARDEN RADLER AGRUM 0,0', "BECK'S REGULAR", 'HORSE ALE', 'LEFFE RADIEUSE', 'GINETTE BLANCHE', 'DIEKIRCH XMAS BEER', 'GINDER-ALE', 'LEFF NECT', 'DIEKIRCH GRAND CRU', 'DEUS', 'SAFIR REGULAR', 'LEFFE MIXED', 'CUBANISTO PHENOMENAL', " GOOSE ISLAND HONKER'S ALE", 'LEFFE ROYALE IPA', 'BIRRA DEL BORGO CASTAGNALE', 'GOOS 312',
+        'GOOSE ISLAND MIDWAY IPA', 'DIEKIRCH BRUIN', 'LEFFE ROYALE MAPUCHE'],key="11")
+        tax = st.number_input(label='Tax',key="13")
+    
+    
+    province = st.selectbox('Province',['Brussels Capital Region', 'Liège', 'Antwerp', 'Namur', 'Limburg', 'Hainaut', 'East Flanders', 'Flemish Brabant', 'West Flanders',
+    'Luxembourg', 'Walloon Brabant'],key="14")
+
     submit_button = st.form_submit_button(label='Submit')
 
 # st.form_submit_button returns True upon form submit
 if submit_button:
-    st.write('hello', text_input)
-    st.write(x, 'squared is', x * x)
+    city_tier_dict = {
+        0:1,
+        1:2,
+        2:4
+    }
+    def get_poc_image(x):
+        if(x=='Premium'):
+            return 1
+        return 0
+    
+    inputs = [
+        total_volume_2019, total_volume_2018,
+        city_tier_dict.get(city_tier), product_volume_2019, gross_turnover_2019-tax,
+        get_poc_image(poc_image), 
+    ]
+    print(inputs)
+    # data = np.array(inputs)
+    # ser = pd.Series(data)   
+    # st.write(ser.shape)
+
+    
+
+
